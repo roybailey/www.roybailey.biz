321702fbed588a598814480164f5bd4251b3046b
Roy Bailey <roybaileybiz@gmail.com>
2024-08-11 14:13:38 +0100

    tweek the background and text

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,7 +15,7 @@ export default function Index() {
       <div className="relative text-gray-800 bg-gray-50">
           <section className="px-10 mt-10">
 
-              <div className="w-full h-screen bg-gray-100 px-2 pt-2">
+              <div className="w-full h-full bg-gray-100 px-2 py-2">
                   <div className="relative mt-16 mb-32 max-w-lg mx-auto mt-24">
 
                       <div className="rounded overflow-hidden shadow-md bg-white">
@@ -24,20 +24,22 @@ export default function Index() {
                                   className="object-cover w-40 h-40 sm:w-60 sm:h-60 lg:w-80 lg:h-80 rounded-full my-5 ring-2 ring-gray-500/50 ring-offset-[10px]"
                                   src="/img/rb-profile-2021.png"
                                   alt="Roy Bailey"/>
-                              <h1 className="text-5xl md:text-6xl lg:text-8xl">Roy Bailey</h1>
+                              <h1 className="text-5xl md:text-6xl lg:text-7xl">Roy Bailey</h1>
                               <p className="text-center text-gray-400 text-sm md:text-base pt-3 px-3 font-normal italic font-sans">
                                   30 years experience Software Engineering...
                               </p>
-                              <ul className="list-none text-center text-gray-600 text-base md:text-lg pt-3 px-3 font-normal">
+                              <ul className="list-none text-center text-gray-600 text-base md:text-lg font-normal pt-3 px-3">
                                   <li>Head of Engineering</li>
                                   <li>Software Engineering Manager</li>
+                                  <li>Running Remote Engineering Offices</li>
+                                  <li><br /></li>
                                   <li>Solutions Architect</li>
                                   <li>Full Stack Developer</li>
                                   <li>Front End Developer</li>
                                   <li>API Developer</li>
                               </ul>
                               <p className="text-center">
-                                  <ul className="list-none flex gap-4 text-center text-gray-600 text-base pt-3 px-3 font-normal">
+                              <ul className="list-none flex gap-4 text-center text-gray-600 text-base pt-3 px-3 font-normal">
                                       <SocialItem link="https://www.linkedin.com/in/roybailey"
                                                   icon='fa-linkedin-square'
                                                   label="LinkedIn Link"/>
@@ -50,7 +52,7 @@ export default function Index() {
                                   </ul>
                               </p>
                               <p className="text-center text-base px-3 pb-4 font-normal flex flex-col gap-2">
-                                  <div className="text-sm md:text-base text-gray-400 italic font-sans">Available or fractal work through my company...</div>
+                                  <div className="text-sm md:text-base text-gray-400 italic font-sans">Available for fractal work through my company...</div>
                                   <a href="https://odinium.com" className="font-bold text-current text-lg md:text-2xl underline">Odinium Ltd</a>
                               </p>
                           </div>
