9b0b8d6426e12f4831da111392261156cbdb3cd1
Roy Bailey <roybaileybiz@gmail.com>
2025-03-15 14:10:53 +0000

    fix icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon";
 
 const ORIGIN_URL = process.env.VITE_ORIGIN || 'https://www.odinium.com';
 
@@ -20,14 +19,6 @@ export default defineConfig({
     },
     integrations: [
         react(),
-        icon({
-            include: {
-                // Include only three `lucide` icons in the bundle
-                lucide: ['fa-linkedin-square','fa-github-square','fa-twitter-square'],
-                // Include all `uis` icons
-                // uis: ['*']
-            }
-        }),
         mdx(),
     ],
 });
